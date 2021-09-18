a7e4cc47b9fd043cea0ad35039ac4975892f95ae
movieshubofficial123 <85605801+movieshubofficial123@users.noreply.github.com>
2021-09-18 07:55:21 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code>"
+    text=f"<i> @movies_Hubh2 \n Found</i> <code>{leng}</code> <i>Results For Your REQUEST:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
